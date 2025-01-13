2fa421b6edd56b49f9d1547963064fddf2a1b833
craig <craigtipple81@gmail.com>
2025-01-13 22:16:16 +0000

    Added google fonts

--- App.js
+++ App.js
@@ -1,16 +1,49 @@
-import { StatusBar } from "expo-status-bar";
+import Home from "./screens/home";
+import * as Font from "expo-font";
+import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello world!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  useEffect(() => {
+    async function loadFonts() {
+      try {
+        await Font.loadAsync({
+          "nunito-regular": require("./assets/fonts/Nunito/static/Nunito-Regular.ttf"),
+          "nunito-bold": require("./assets/fonts/Nunito/static/Nunito-Bold.ttf"),
+        });
+        setFontsLoaded(true);
+      } catch (error) {
+        console.error("Error loading fonts:", error);
+      }
+    }
+
+    loadFonts();
+  }, []);
+
+  if (fontsLoaded) {
+    return (
+      <View style={styles.container}>
+        <Home />
+      </View>
+    );
+  } else {
+    return (
+      <View style={styles.loadingContainer}>
+        <Text>Loading...</Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
+  loadingContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
+  },
   container: {
     flex: 1,
     backgroundColor: "#fff",
---
+++ screens/home.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.titleText}>Home component</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  titleText: {
+    fontFamily: "nunito-bold", // Make sure this matches the one used in Font.loadAsync
+    fontSize: 18,
+  },
+});
