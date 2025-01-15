7aa248bf2c678e7fb230b2cf01b21721788a1d60
craig <craigtipple81@gmail.com>
2025-01-15 12:30:55 +0000

    Updated register.js with form inputfields.

--- screens/authentication/register.js
+++ screens/authentication/register.js
@@ -1,9 +1,54 @@
 import react from "react";
-import { View, Text, Button } from "react-native";
+import { globalStyles } from "../../styles/global";
+import { View, Text, TextInput, Button } from "react-native";
+import { Formik } from "formik";
+import * as Yup from "yup";
 
 const Register = ({ navigation }) => {
   return (
-    <View>
+    <View style={globalStyles.container}>
+      <Formik
+        initialValues={{
+          email: "",
+          password: "",
+          username: "",
+          location: "",
+        }}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+      >
+        {(formikProps) => (
+          <View>
+            <TextInput
+              style={globalStyles.input}
+              placeholder="Email"
+              onChangeText={formikProps.handleChange("email")}
+              value={formikProps.values.email}
+            />
+            <TextInput
+              style={globalStyles.input}
+              placeholder="Password"
+              onChangeText={formikProps.handleChange("password")}
+              value={formikProps.values.password}
+              secureTextEntry
+            />
+            <TextInput
+              style={globalStyles.input}
+              placeholder="Username"
+              onChangeText={formikProps.handleChange("username")}
+              value={formikProps.values.username}
+            />
+            <TextInput
+              style={globalStyles.input}
+              placeholder="Location"
+              onChangeText={formikProps.handleChange("location")}
+              value={formikProps.values.location}
+            />
+            <Button title="Submit" onPress={formikProps.handleSubmit} />
+          </View>
+        )}
+      </Formik>
       <Text>Already have an account?</Text>
       <Button title={"Sign In"} onPress={() => navigation.replace("SignIn")} />
     </View>
